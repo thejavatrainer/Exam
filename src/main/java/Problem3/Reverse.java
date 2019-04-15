671efb20a6646f78fe1fe97ca0a6181d4d128f14
ioanamacarie <ioana-maria.macarie@frequentis.com>
2019-04-15 11:30:49 +0300

    5 commit

--- src/main/java/Problem3/Reverse.java
+++ src/main/java/Problem3/Reverse.java
@@ -4,4 +4,20 @@ package Problem3;
  * Created by imacarie on 15.04.2019.
  */
 public class Reverse {
+
+    public static void main(String[] args) {
+        String str = "Macarie Ioana Maria";
+        char[] charArray = str.toCharArray();
+        int indexOfLastChar = charArray.length - 1;
+        for (int i = 0; i < charArray.length / 2; i++) {
+            char temp = charArray[i];
+            charArray[i] = charArray[indexOfLastChar - i];
+            charArray[indexOfLastChar - i] = temp;
+
+        }
+        String reversedStr = new String((charArray));
+        System.out.println("The reversed String is: "  + reversedStr);
+
+    }
 }
+
