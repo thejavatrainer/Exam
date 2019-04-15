8a9299094856c6b548b01979226882858505fc0a
amuica <Piticotu_1>
2019-04-15 11:57:10 +0300

    Four exercise

--- exam_1/src/main/java/Piramida.java
+++ exam_1/src/main/java/Piramida.java
@@ -1,10 +1,13 @@
 public class Piramida {
     public static void printPiramida(int n) {
         if ((n & 1) == 1) {
-            for (int i = 1; i <= (n / 2 + 1); i ++) {
+            for (int i = 1; i <= n;i=i+2) {
+                for (int j = 0; j < (n-i)/2; j++) {
+                    System.out.print(" ");
+                }
                 for (int j = 0; j < i; j++) {
                     System.out.print("#");
-                }
+            }
                 System.out.println("");
             }
         }
@@ -12,6 +15,6 @@ public class Piramida {
     }
     public static void main(String args[])
     {
-        printPiramida(7);
+        printPiramida(5);
     }
 }
