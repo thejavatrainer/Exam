8d9fe5347909177ffbb097ad28d1b51e7f2d7bf1
amuica <Piticotu_1>
2019-04-24 16:29:55 +0300

    Update

--- exam_1/src/main/java/Person.java
+++ exam_1/src/main/java/Person.java
@@ -1,3 +1,4 @@
+//import  java.lang.IllegalAccessException;
 public class Person {
     public String name;
     public int age;
@@ -30,37 +31,45 @@ public class Person {
         System.out.println(" Varsta medie este:" + result / sir.length);
     }
 
-    public static void getYoungestOldest(Person[] sir) {
-        int youngest = sir[0].age;
-        int oldest = sir[0].age;
-        int youngestI = 0;
-        int oldestI = 0;
-        for (int i = 1; i < sir.length; i++) {
-            if (sir[i].age < youngest) {
-                youngest = sir[i].age;
-                youngestI = i;
-
-            }
-            if (sir[i].age > oldest) {
-                oldest = sir[i].age;
-                oldestI = i;
-
-            }
+    public static Person getYoungest(Person[] sir) throws IllegalAccessException {
+        if (sir == null || sir.length == 0) {
+            throw new IllegalAccessException(" No persons specifed");
         }
-        System.out.println("Cea mai tanara persoana este: " + sir[youngestI].name);
-        System.out.println("Cea mai batrana persoana este: " + sir[oldestI].name);
+        Person youngest = sir[0];
+        for (Person p : sir) {
+            if (p.age < youngest.age) {
+                youngest = p;
 
+            }
+
+        }
+        return youngest;
     }
 
-    public static boolean isMajor(Person Persoana) {
+    public static Person getOldest(Person[] sir) throws IllegalAccessException {
+        if (sir == null || sir.length == 0) {
+            throw new IllegalAccessException(" No persons specifed");
+        }
+        Person oldest = sir[0];
+        for (Person p : sir) {
+            if (p.age > oldest.age) {
+                oldest = p;
 
-        return Persoana.age >= 18;
+            }
+
+        }
+        return oldest;
+    }
+
+    public boolean isMajor() {
+
+        return age >= 18;
     }
 
     public static Person[] isMajorAll(Person[] sir) {
         int j = 0;
         for (int i = 0; i < sir.length; i++) {
-            if (isMajor(sir[i])) {
+            if (sir[i].isMajor()) {
                 j++;
             }
         }
@@ -68,7 +77,7 @@ public class Person {
         Person[] result = new Person[j];
         j = 0;
         for (int i = 0; i < sir.length; i++) {
-            if (isMajor(sir[i])) {
+            if ((sir[i].isMajor())) {
                 result[j] = new Person(sir[i].name, sir[i].age);
                 j++;
             }
@@ -78,16 +87,28 @@ public class Person {
 
     public static void main(String Args[]) {
         Person[] sirPersoane = new Person[3];
+        Person[] sirPersoaneEmpty = new Person[0];
         sirPersoane[0] = new Person("Alex", 8);
         sirPersoane[1] = new Person("Adrian", 28);
         sirPersoane[2] = new Person("Ana", 18);
         System.out.println(" Ana se afla printre persoane:" + containAna(sirPersoane));
         doAverageAge(sirPersoane);
-        getYoungestOldest(sirPersoane);
+        try {
+            System.out.println(" Persoana cea mai tanara este " + getYoungest(sirPersoane).name + " cu varsta de " + getYoungest(sirPersoane).age);
+        } catch (IllegalAccessException myException) {
+            //fa ceva .....
+        }
+
+        try {
+            System.out.println(" Persoana cea mai batrana este " + getOldest(sirPersoane).name + " cu varsta de " + getOldest(sirPersoane).age);
+        } catch (IllegalAccessException myException) {
+            //fa ceva .....
+        }
+
         Person[] sirPersoanemMajor = isMajorAll(sirPersoane);
         System.out.println("Toate personele majore: ");
         for (int i = 0; i < sirPersoanemMajor.length; i++) {
-            System.out.println("Persona "+sirPersoanemMajor[i].name+" are varsta de "+sirPersoanemMajor[i].age);
+            System.out.println("Persona " + sirPersoanemMajor[i].name + " are varsta de " + sirPersoanemMajor[i].age);
         }
     }
 }
